ca49ad4857eb4288df3519be9651e5ac56184dce
Jessica Bealey <jfbealey@gmail.com>
2023-06-18 00:46:55 +0100

    frame class logs a strike or a spare

--- frame.js
+++ frame.js
@@ -1,17 +1,38 @@
 class Frame {
     constructor(){
-        this.scoreCount = 0
+        this.scoreCount = 0;
+        this.rollCount = 1;
+        this.isStrike = false;
+        this.isSpare = false;
 
     };
 
     roll(pins){
-        this.scoreCount += pins;
+        if(this.rollCount==1 && pins ==10){
+            this.isStrike = true;
+            this.scoreCount += pins;
+        } else if(this.rollCount == 2 && this.scoreCount + pins == 10){
+            this.isSpare = true;
+            this.scoreCount += pins;   
+        }else {
+            this.scoreCount += pins; 
+            this.rollCount ++;
+        }
+
     }
 
     score(){
         return this.scoreCount;
     }
 
+    strike(){
+        return this.isStrike;
+    };
+
+    spare(){
+        return this.isSpare;
+    }
+    
 };
 
 module.exports = Frame;
--- frame.test.js
+++ frame.test.js
@@ -1,11 +1,34 @@
 const Frame = require('./frame');
 
 describe('Frame', () =>{
-    it('Scores a single roll', () =>{
+    it('Scores a single roll', () => {
         const frame = new Frame();
         frame.roll(5);
         expect(frame.score()).toBe(5);
-
     });
+
+    it('Scores two rolls', () => {
+        const frame = new Frame();
+        frame.roll(5);
+        frame.roll(3);
+        expect(frame.score()).toBe(8);
+    });
+
+    it('logs a strike', () => {
+        const frame = new Frame();
+        frame.roll(10);
+        expect(frame.score()).toBe(10);
+        expect(frame.strike()).toBe(true);
+    })
+
+    it('logs a spare', () => {
+        const frame = new Frame();
+        frame.roll(5);
+        frame.roll(5);
+        expect(frame.score()).toBe(10);
+        expect(frame.spare()).toBe(true);
+
+    })
+
 
 });
